5b0d29cd8f0034427292dd1ef9411902261d462a
ProstoSo <sofya.pecherkina@gmail.com>
2021-01-30 13:23:32 +0700

    made new branch

---
+++ augs/test/test_aug_abbr.py
@@ -0,0 +1,11 @@
+import unittest
+
+from augs.aug_abbreviation import AugCloseAbbr, AugOpenAbbr
+
+class TestAugCloseAbbr(unittest.TestCase):
+
+    def setUp(self) :
+        self._augmentation=AugCloseAbbr
+
+    def test_augmentation(self):
+        pass
