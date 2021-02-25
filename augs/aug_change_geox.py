331ec7713474f7c66cb0e1dcf98e4caf50c3769e
Bruches <bruches@bk.ru>
2021-02-25 17:46:16 +0700

    оптимизирован change_geox

--- augs/aug_change_geox.py
+++ augs/aug_change_geox.py
@@ -1,36 +1,37 @@
-import json
-import random
 import re
 import os
+import json
+import random
 
 import pymorphy2
 
-from .base_aug import BaseAug
-from .paths import FILES_PATH
+from augs.base_aug import BaseAug
+from augs.paths import FILES_PATH
+from augs.utils import remove_whitespace
 
 
-#аугментация, которая заменяет одни географические названия другими, сохраняя тип топонима
 class AugChangeGeox(BaseAug):
+    """ Аугментация, которая заменяет одни географические названия другими, сохраняя тип топонима """
 
     def __init__(self):
-        with open (os.path.join(FILES_PATH, 'geox.json'),'r',encoding='utf-8') as geoxs:
-            self._geoxs=json.load(geoxs)
-        self._morph=pymorphy2.MorphAnalyzer()
+        with open(os.path.join(FILES_PATH, 'geox.json'), 'r', encoding='utf-8') as geoxs:
+            self._geoxs = json.load(geoxs)
+        self._morph = pymorphy2.MorphAnalyzer()
 
     def apply(self, text: str):
-        text = text.split(" ")
-        for word in text:
-            s=""
+        tokens = text.split(' ')
+        for token in tokens:
+            s = ''
             # ищем слово с заглавной буквы
-            if word.istitle():
-                for symb in [",", ".", "!", "?"]:
-                    if symb in word:
-                        word = word.replace(symb, "")
-                        s=symb
+            if token.istitle():
+                for symb in [',', '.', '!', '?']:
+                    if symb in token:
+                        token = token.replace(symb, '')
+                        s = symb
                 # проверяем, является найденное слово географическим названием
-                firstword = self._morph.parse(word)[0]
-                if "Geox" in firstword.tag:
-                    newword = word
+                firstword = self._morph.parse(token)[0]
+                if 'Geox' in firstword.tag:
+                    newword = token
                     # запоминаем исходный падеж
                     case = firstword.tag.case
                     # для поиска слова в списке геогр.названием выбирапм форму Им.Падежа и пишем ей с заглавной буквы
@@ -41,25 +42,27 @@ class AugChangeGeox(BaseAug):
                             newword = random.choice(names)
                             break
                     #если в словаре не нашлось такого географического названия, то мы его пропускаем
-                    if newword == word:
+                    if newword == token:
                         continue
                     # загружаем новое слово в pymorphy, чтобы получить нужную форму
                     secondword = self._morph.parse(newword)[0]
-                    # проверяем "совмеестимость" предлога
-                    if text[text.index(word+s) - 1].lower() in ["в", "во"]:
+                    # проверяем "совместимость" предлога
+                    previous_token_index = tokens.index(token + s)
+                    if tokens[previous_token_index - 1].lower() in ['в', 'во']:
                         case = 'loct'
                         reg = re.compile("^[В|Ф][^аоуэиыяеёю]")
                         result = re.match(reg, newword)
-                        prword = text[text.index(word+s) - 1]
+                        prword = tokens[previous_token_index - 1]
                         if result != None:
-                            prword = prword + "о"
+                            prword = prword + 'о'
                         else:
                             prword = prword[:1]
-                        text[text.index(word+s) - 1] = prword
+                        tokens[previous_token_index - 1] = prword
                         # выбираем нужную форму слова и меняем первую букву слова на заглавную
-                    newword1 = secondword.inflect({case}).word.capitalize()+s
+                    newword1 = secondword.inflect({case}).word.capitalize() + s
                     # заменяем старое слово на новое
-                    n = text.index(word+s)
-                    text[n] = newword1
-        newtext = " ".join(text)
+                    n = previous_token_index
+                    tokens[n] = newword1
+        newtext = ' '.join(tokens)
+        newtext = remove_whitespace(newtext)
         return newtext
